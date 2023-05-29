076a052494a9f89eccff9157927e9d412d5f3df2
wellington.xavier <sidious.lords@gmail.com>
2023-05-29 14:04:18 -0300

    nova tela de game over
    criei uma nova tela de fim de jogo

--- app/validacao.js
+++ app/validacao.js
@@ -1,11 +1,27 @@
 function verificaValorValido(chute) {
     const numero = +chute
 
+    // if (chuteForInvalido(numero)) {
+    //     elementoChute.innerHTML += `
+    //     <div>valor invalido</div>
+    //     `
+    //     return
+    // }
+
     if (chuteForInvalido(numero)) {
-        elementoChute.innerHTML += `
-        <div>valor invalido</div>
-        `
-        return
+        if (chute.toUpperCase() === "GAME OVER") {
+
+            document.body.innerHTML =
+                `
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+                `
+                document.body.style.backgroundColor = "black";
+        } else {
+
+            elementoChute.innerHTML += '<div>Valor Inválido</div>';
+        }
     }
 
     if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
@@ -44,3 +60,5 @@ document.body.addEventListener('click', e => {
         window.location.reload()
     }
 })
+
+
